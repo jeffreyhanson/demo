ac159ff5414dac41b644802b73f25ca3223b8e6d
Jeff Hanson <jeffrey.hanson@uqconnect.edu.au>
2018-04-26 15:02:49 +1000

    change modle

--- code/model.R
+++ code/model.R
@@ -2,4 +2,4 @@
 data <- read.csv("../data/formatted.csv")
 
 # run some model
-model <- glm(value ~ Species, data = data, family = "poisson")
+model <- glm(value ~ Species, data = data, family = "binomial")
